6ef93634d00c1cf9ee6a623439bd7d55b0f1584e
Maryam Rostamali <maryam.rostamali91@gmail.com>
2022-02-16 15:07:51 -0700

    Registering the service and building the back end

---
+++ src/ChinookSolution/ChinookSystem/ChinookExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ChinookSystem.DAL;
+using ChinookSystem.BLL;
+#endregion
+
+namespace ChinookSystem
+{
+    public static class ChinookExtensions
+    {
+        public static void ChinookSystemBackendDependencies
+            (this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            //Register the DBContext class in Chinook with the service collection
+            services.AddDbContext<ChinookContext>(options);
+
+            //Add any services that you create in the class library
+            //   using the .AddTransient<T>(...)
+
+            services.AddTransient<AboutServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetRequiredService<ChinookContext>();
+                //Create an instance of the services and return the instace
+                return new AboutServices(context);
+            });
+        }
+    }
+}
--- src/ChinookSolution/WebApp/Program.cs
+++ src/ChinookSolution/WebApp/Program.cs
@@ -2,12 +2,42 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
 
+#region Additional Namespaces
+using ChinookSystem;
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Given
+//supplied database connection due to the fact that we create this web app 
+//  to use Individual Accounts
+//Code retreives the connection string from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Added
+//Code retreived the ChinookDB connection string 
+var connectionStringChinook = builder.Configuration.GetConnectionString("ChinookDB");
+
+//Given
+//Register the supplied connection string with the IServiceCollection (.Services)
+//Registers the connection string for Individual Accounts 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+//Added
+//Code the logic to add our class library services to IServiceCollection
+//One could do the registration code here in Program.cs
+//HOWEVER, every time a service class is added, you would be changing this file 
+//The implementation of the DBContent and AddTransient(..) code in this example 
+//   will be done in an extension method to IServiceCollection 
+//The extension method will be coded inside the ChinookSystem class library 
+//The extension method will have a parameter: and this paramater is and options => options.UseSqlServer()
+builder.Services.ChinookSystemBackendDependencies(options =>
+    options.UseSqlServer(connectionStringChinook));
+
+//Given
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
